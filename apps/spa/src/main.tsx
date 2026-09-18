f70adaf8043448ffe007aab16cb1ee90c6e8eacf
patrickl-ma <patrickl.ma@gmail.com>
2026-09-17 18:02:11 -0700

    mantine setup

--- apps/spa/src/main.tsx
+++ apps/spa/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+// Mantine setup
+import { createTheme, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+// import '@mantine/dates/styles.css';
+// import '@mantine/dropzone/styles.css';
+// import '@mantine/code-highlight/styles.css';
+// ...
+const theme = createTheme({});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </StrictMode>,
-)
+);
